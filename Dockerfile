8af7af874cac0ea5ac48aa125c3ac7aaa3536723
Nick Cox <nickcox1008@gmail.com>
2015-02-23 18:14:54 -0700

    andrew helped me make it better

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,7 @@
 FROM debian:wheezy
 
-RUN useradd --create-home user
-
 RUN apt-get update && apt-get install -y \
 		audacity \
 	--no-install-recommends && rm -rf /var/lib/apt/lists/*
-
-USER user
-VOLUME /home/user
 
 CMD ["audacity"]
--- run.sh
+++ run.sh
@@ -2,9 +2,12 @@
 set -e
 
 docker run --rm \
+	-u $(id -u):$(id -g) \
 	-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 	-v /dev/snd:/dev/snd \
-	-v ~:/home/user \
+	-v "$HOME:$HOME" \
+	-w "$HOME" \
 	-e DISPLAY="unix$DISPLAY" \
+	-e HOME \
 	$(find /dev/snd/ -type c | sed 's/^/--device /') \
 	knickers/audacity
